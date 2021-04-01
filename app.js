6d01c011db7af7d3cc54ceec3c9e2891f377dfcb
Fatih <77235421+fatihcnn@users.noreply.github.com>
2021-04-01 21:54:22 +0300

    design changes

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const screenBody = document.querySelector(".screen-body");
 const scrollCheck = document.querySelector(".scroll-check");
 const topBar = document.querySelector(".screen-top");
 const bottomBar = document.querySelector(".screen-bottom");
+const width = document.querySelectorAll(".content").offsetWidth;
+const slider = document.querySelector(".imp-news");
 
 screenBody.addEventListener("scroll", function () {
   if (scrollCheck.getBoundingClientRect().top > scrollPos) {
